701c21c7d6dbf5da8ac513986660459fdafc30ed
RayO10 <raymon.ortiz4647@alumnos.udg.mx>
2023-02-08 23:25:08 -0600

    v0.0.1 Se agrega inicio de sesion y bootstrap

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="BS/styles.css" />
+</head>
+
+<body>
+    <h3>Inicia Sesión<h3>
+            <form action="inicio.php" method="post">
+                <input placeholder="Usuario" id="usuario"></input>
+                <br>
+                <input placeholder="Contraseña" id="contraseña"></input>
+                <br>
+                <button type="submit" class="btn btn-dark">Enviar</button>
+                <br>
+                <a href="registro.php"> Registrarte aquí</a>
+            </form>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+                integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+                crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ registro.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="BS/styles.css" />
+</head>
+
+<body>
+    <h3>Nueva Cuenta</h3>
+    <form>
+        <input placeholder="Usuario" id="usuario"></input>
+        <br>
+        <input placeholder="Nombre Completo" id="nombre"></input>
+        <br>
+        <input placeholder="Correo" id="correo"></input>
+        <br>
+        <input placeholder="Descripción" id="descripcion"></input>
+        <br>
+        <button>Enviar</button>
+        <br>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
