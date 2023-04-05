3133f8854c9ce963ddd8b1829a1f81b53ebc60d7
Axel Morales <axel@moralestorres.dev>
2023-04-04 19:12:43 -0600

    Arreglado error de nombre

--- api/plataformas/modificacion.php
+++ api/plataformas/modificacion.php
@@ -3,6 +3,6 @@ require_once('../.includes/db_conn.inc.php');
 require_once('../.includes/session.inc.php');
 header('Content-Type: application/json');
 $stmt = $conn->prepare('UPDATE plataformas SET nombre=? WHERE id=?');
-$stmt->bind_param('si', $_POST['nombre'], $_POST['nombre']);
+$stmt->bind_param('si', $_POST['nombre'], $_POST['id']);
 $stmt->execute();
 echo json_encode(array('exito' => $stmt->affected_rows == 1));
