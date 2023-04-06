0ddfd2bb42722484e71b5a0a7fc7047cd5519686
Axel Morales <axel@moralestorres.dev>
2023-04-06 13:54:41 -0600

    Utilizando hash de contraseñas

--- api/usuarios/alta.php
+++ api/usuarios/alta.php
@@ -2,6 +2,7 @@
 require_once('../.includes/db_conn.inc.php');
 require_once('../.includes/session.inc.php');
 header('Content-Type: application/json');
+$_POST['contrasena'] = password_hash($_POST['contrasena'], PASSWORD_DEFAULT);
 $stmt = $conn->prepare('INSERT INTO usuarios(usuario, correo, nombre, contrasena, descripcion) VALUES(?,?,?,?,?)');
 $stmt->bind_param('sssss', $_POST['usuario'], $_POST['correo'], $_POST['nombre'], $_POST['contrasena'], $_POST['descripcion']);
 $stmt->execute();
--- api/usuarios/modificacion.php
+++ api/usuarios/modificacion.php
@@ -2,6 +2,7 @@
 require_once('../.includes/db_conn.inc.php');
 require_once('../.includes/session.inc.php');
 header('Content-Type: application/json');
+$_POST['contrasena'] = password_hash($_POST['contrasena'], PASSWORD_DEFAULT);
 $stmt = $conn->prepare('UPDATE usuarios SET usuario=?, correo=?, nombre=?, contrasena=?, descripcion=? WHERE id=?');
 $stmt->bind_param('sssssi', $_POST['usuario'], $_POST['correo'], $_POST['nombre'], $_POST['contrasena'], $_POST['descripcion'], $_POST['id']);
 $stmt->execute();
