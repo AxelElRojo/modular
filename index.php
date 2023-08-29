be70438266a3d7cb79ad1367906630a81a21f0b6
Axel Morales Torres <axelzkai.123@gmail.com>
2023-08-28 23:31:38 -0600

    Más chingaderas

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
 include_once('./api/.includes/session.inc.php');
-if(sesion_existe())
-	readfile('inicio.php');
+if(sesion_existe()){
+	if(isset($_SESSION['noterminado'])){
+		switch($_SESSION['noterminado']){
+			case 'hobbies':
+				readfile('views/agregarHobbies.html');
+			break;
+			case 'plataformas':
+				readfile('views/agregarPlataformas.html');
+			break;
+			case 'videojuegos':
+				readfile('views/agregarVideojuegos.html');
+			break;
+			default:
+				readfile('views/inicio.html');
+		}
+	}else
+		readfile('views/inicio.html');
+}
 else
-	readfile('login.html');
+	readfile('views/login.html');
